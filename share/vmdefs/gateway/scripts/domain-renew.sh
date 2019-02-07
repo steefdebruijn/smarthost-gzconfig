d1e27910bcdce9fe886a4a740a8dc8e3c6900d5d
Steef de Bruijn <steef@debruijn.ws>
2019-02-07 23:02:29 +0100

    SSL chain leftovers removed

--- share/vmdefs/gateway/scripts/domain-renew.sh
+++ share/vmdefs/gateway/scripts/domain-renew.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-if test ! -f ${1}.chain.pem || echo $1 | grep local > /dev/null; then
+if test ! -f ${1}.domain.crt || echo $1 | grep local > /dev/null; then
     echo "Signing LOCAL"
     openssl x509 -req -days 365 \
 	    -in /opt/local/etc/nginx/certs/${1}.domain.csr \
 	    -signkey /opt/local/etc/nginx/certs/${1}.domain.key \
 	    -out /opt/local/etc/nginx/certs/${1}.domain.crt
-    cp /opt/local/etc/nginx/certs/${1}.domain.crt \
-       /opt/local/etc/nginx/certs/${1}.chain.pem
 else
     echo "Signing through Lets Encrypt"
     acme_tiny --account-key /opt/local/etc/nginx/certs/${1}.account.key \
